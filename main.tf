146bc765d986d69c6f3b790fdabe0012edb005fa
Toby <tobyclemson@gmail.com>
2022-11-30 11:45:11 -0300

    Add some notes.

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 resource "aws_vpc_endpoint" "vpc_endpoint" {
   vpc_id       = var.vpc_id
   service_name = "com.amazonaws.${var.region}.s3"
+
+  subnet_ids = []
 }
+
+#resource "aws_vpc_endpoint_subnet_association" "" {
+#
+#}
+
+// Use correct service name as requested by module consumer
+// Add tags -> component, deployment identifier
+// Associate with subnets as requested by module consumer
+// ?? Do we need a security group?
+// ?? Do we need a policy?
